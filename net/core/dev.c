f0d795b38903a79defce17b84c75753c99062db2
John Galt <johngaltfirstrun@gmail.com>
2016-05-01 17:59:15 -0700

    disable mac randomization by default

--- net/core/dev.c
+++ net/core/dev.c
@@ -178,7 +178,7 @@
 static DEFINE_SPINLOCK(ptype_lock);
 static struct list_head ptype_base[PTYPE_HASH_SIZE] __read_mostly;
 static struct list_head ptype_all __read_mostly;	/* Taps */
-int randomize_mac = 1;
+int randomize_mac = 0;
 
 /*
  * The @dev_base_head list is protected by @dev_base_lock and the rtnl
